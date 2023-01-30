4a25f5ff8ad36575ccba754d0f175a720d2f1151
Alex <rozenberg.s@mail.ru>
2023-01-30 23:42:06 +0300

    First commit

---
+++ src/master.java
@@ -0,0 +1,8 @@
+public class master {
+    public static void main(String[] args) {
+        int num = 1;
+        int num2 = 2;
+        int num3 = 5;
+        System.out.println(num + num2 + num3);
+    }
+}
