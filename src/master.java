129db117dbeeab8d0f23d2cd9ba6eb6b5e2b3a99
Alex <rozenberg.s@mail.ru>
2023-01-30 23:47:40 +0300

    All done

--- src/master.java
+++ src/master.java
@@ -4,6 +4,6 @@ public class master {
         int num2 = 2;
         int num3 = 5;
         int num4 = 20;
-        System.out.println(num + num2 + num3 + num4);
+        System.out.println(num + num2 + num3 - num4);
     }
 }
